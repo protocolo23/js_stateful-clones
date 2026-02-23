9ba88b88388888397148c02000e8a78dca53879c
protocolo23 <narashikamaro27@gmail.com>
2026-02-22 21:45:07 -0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  // eslint-disable-next-line spaced-comment
+  //write code here
+  const result = [];
+  let current = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        current = {};
+        break;
+      case 'addProperties':
+        current = { ...current, ...action.extraData };
+        break;
+      case 'removeProperties':
+        current = { ...current };
+
+        for (const key of action.keysToRemove) {
+          delete current[key];
+        }
+        break;
+      default:
+        break;
+    }
+    result.push(current);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
